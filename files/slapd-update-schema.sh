a15aa9c352756fbdf6b36d0166a9ca42bee992fa
Yuriy Shestakov <yshestakov@gmail.com>
2016-04-11 22:54:56 +0300

    Fixed slapd-update-schema.sh script

--- files/slapd-update-schema.sh
+++ files/slapd-update-schema.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 # https://wiki.debian.org/LDAP/OpenLDAPSetup
 
 # /etc/init.d/slapd stop
 # slapindex
 # chown -R openldap:openldap /var/lib/ldap
 # /etc/init.d/slapd start
+WDIR=$(dirname $(readlink -f "$0" ))
 
-ldapmodify -Y EXTERNAL -H ldapi:/// -f /var/lib/juju/olcDbIndex.ldif
-ldapmodify -Y EXTERNAL -H ldapi:/// -f /var/lib/juju/olcAccess.ldif
+ldapmodify -Y EXTERNAL -H ldapi:/// -f $WDIR/olcDbIndex.ldif
+ldapmodify -Y EXTERNAL -H ldapi:/// -f $WDIR/olcAccess.ldif
 
 # copy modified schema files
 cp -f core.schema inetorgperson.schema /etc/ldap/schema/
 test -d /tmp/slapd.d && rm -rf /tmp/slapd.d
 mkdir /tmp/slapd.d
-slaptest -f /var/lib/juju/slapd-test.conf -F /tmp/slapd.d/
+slaptest -f $WDIR/slapd-test.conf -F /tmp/slapd.d/
 DST="/etc/ldap/slapd.d/cn=config/cn=schema"
 SRC="/tmp/slapd.d/cn=config/cn=schema"
 # cp -p $SRC/*.ldif $DST
